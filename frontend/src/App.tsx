004e8c61e27efc75dd099b34b30a4d54584bbae3
William Glory <williamgloryh@gmail.com>
2024-11-25 21:18:51 +0700

    feat: add navbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
-import Footer  from "./components/footer";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 import SignUpPage from "./pages/auth/signup";
 import LoginPage from "./pages/auth/login";
 import { Toaster } from "react-hot-toast";
@@ -11,20 +12,23 @@ import UserList from "./pages/connection/user-list";
 function App() {
   return (
     <Router>
-        <main className="container mx-auto px-4 bg-">
-            <Routes>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="container mx-auto px-4 flex-grow">
+          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<SignUpPage />} />
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/invitation" element={<Invitations/>} />
-            <Route path="/connections/:userId" element={<Connections/>} />
+            <Route path="/invitation" element={<Invitations />} />
+            <Route path="/connections/:userId" element={<Connections />} />
             <Route path="/users" element={<UserList/>} />
-            </Routes>
-            <Toaster/>
+          </Routes>
+          <Toaster />
         </main>
         <Footer />
+      </div>
     </Router>
   );
 }
 
-export default App
+export default App;
