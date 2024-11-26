c747213bc17743865d0b7b4bc620d6ff21df0eff
Rayhan Fadhlan <rayhanfazka07@gmail.com>
2024-11-26 23:07:06 +0700

    feat : integrate profile page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,44 +11,56 @@ import { apiReference } from "@scalar/hono-api-reference";
 import userRouter from "./routes/user.route.js";
 import { cors } from "hono/cors";
 import { connectionRouter } from "./routes/connection.route.js";
+import { serveStatic } from "@hono/node-server/serve-static";
 
+const main = new OpenAPIHono();
 
-const app = new OpenAPIHono().basePath("/api");
+const app = main.basePath("/api");
 
-app.use(logger())
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}))
+main.use(
+  "/uploads/*",
+  serveStatic({root: "./"})
+);
+
+app.use(logger());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    exposeHeaders: ["Location"],
+  })
+);
 app.route("/", authRouter);
 app.route("/", profileRouter);
 app.route("/", userRouter);
 app.route("/", connectionRouter);
 
-app.openAPIRegistry.registerComponent('securitySchemes', 'auth', {
-  type: 'http',
-  scheme: 'bearer',
+app.openAPIRegistry.registerComponent("securitySchemes", "auth", {
+  type: "http",
+  scheme: "bearer",
 });
 
-
-
-
-app.openAPIRegistry
+app.openAPIRegistry;
 
 app.onError((err, c) => {
-  if(err instanceof HttpError || err instanceof HTTPException) {
-    return c.json({
-      success : false,
-      message : err.message,
-      error : err.message
-    }, err.status)
-  }
-  else {
-    return c.json({
-      success: false,
-      message: "Internal server error",
-      error: err.message
-    }, 500)
+  if (err instanceof HttpError || err instanceof HTTPException) {
+    return c.json(
+      {
+        success: false,
+        message: err.message,
+        error: err.message,
+      },
+      err.status
+    );
+  } else {
+    return c.json(
+      {
+        success: false,
+        message: "Internal server error",
+        error: err.message,
+      },
+      500
+    );
   }
 });
 
@@ -57,19 +69,15 @@ app.doc("/openapi", {
   info: {
     version: "1.0.0",
     title: "LockedIn",
-   
   },
   security: [
     {
       auth: [],
     },
   ],
-  
 });
 
-app.get('/docs', swaggerUI({ url: '/api/openapi' }))
-
-
+app.get("/docs", swaggerUI({ url: "/api/openapi" }));
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
--- backend/src/repositories/feed.repository.ts
+++ backend/src/repositories/feed.repository.ts
@@ -2,10 +2,15 @@ import { prisma } from "../lib/prisma.js";
 
 export const getFeedsByUserId = async (userId : string) => {
   const id = parseInt(userId);
-  return prisma.feed.findMany({
+  const feeds = await prisma.feed.findMany({
     where: {
       user_id: id
     }
   });
+  return feeds.map(feed => ({
+    ...feed,
+    id: Number(feed.id),
+    user_id: Number(feed.user_id)
+  }));
 
 }
