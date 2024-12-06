3efd4473af67e3dd5ffecc508a3d5ac17c7deaf2
William Glory <williamgloryh@gmail.com>
2024-12-06 23:27:54 +0700

    fix: fix chat frontend

--- frontend/src/components/profile-widget.tsx
+++ frontend/src/components/profile-widget.tsx
@@ -5,7 +5,7 @@ export function ProfileWidget() {
   const { user } = useAuth();
 
   return (
-    <div className="artdecoCard w-full md:w-[225px] overflow-hidden flex flex-col h-fit top-[95px] sticky top-4">
+    <div className="artdecoCard w-full md:w-[225px] overflow-hidden flex flex-col h-fit top-[95px] sticky">
       <div className="relative h-16 bg-[url('/profile-banner.svg')] bg-cover bg-center">
         <div className="absolute -bottom-8 left-4">
           <Avatar
@@ -17,7 +17,7 @@ export function ProfileWidget() {
       </div>
       <div className="flex flex-col pt-10 px-4 pb-4">
         <h2 className="text-md font-semibold">
-          <div className="hover:underline">{user?.name}</div>
+          <div className="cursor-default">{user?.name}</div>
         </h2>
         <span className="text-xs text-muted-foreground">{user?.username}</span>
         <span className="text-xs text-muted-foreground truncate max-w-[140px]">
