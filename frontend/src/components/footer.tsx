c6cf2123a98b3e68040052f2ff8e35de0e74aa3f
Moses-ui <13520052@std.stei.itb.ac.id>
2024-12-08 18:54:06 +0700

    style: adjust footer text and image size

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -4,14 +4,14 @@ import logo from "../assets/images/logo.png";
 const Footer: React.FC = () => {
   return (
     <footer className="bg-white py-4 text-sm text-gray-700 mt-8">
-      <div className="max-w-full mx-auto flex flex-wrap items-center justify-center">
+      <div className="max-w-full mx-auto flex flex-wrap items-center justify-center text-xs">
         <div className="flex items-center mr-0 md:mr-7">
-          <img src={logo} alt="Logo" className="h-8 w-24 mr-1 cursor-default" />
+          <img src={logo} alt="Logo" className="h-6 w-21 cursor-default" />
           <span className="cursor-default">
             &copy; {new Date().getFullYear()}
           </span>
         </div>
-        <div className="flex flex-wrap items-center justify-center gap-6 md:gap-10 mt-4 md:mt-0">
+        <div className="flex flex-wrap items-center justify-center gap-3 md:gap-10 mt-4 md:mt-0">
           <span className="cursor-default">User Agreement</span>
           <span className="cursor-default">Privacy Policy</span>
           <span className="cursor-default">Community Guidelines</span>
